5bf91fb29dfe92ff871463b924af83313f40dd5f
IHIaadj <fh_benmeziane@esi.dz>
2021-01-25 10:14:50 +0100

    general_bench_structure

---
+++ nas_benchmarks.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pickle
+import sys
+import os
+
+# NAS-Bench-101
+from nasbench import api
+
+# NAS-Bench-201
+from nas_201_api import NASBench201API as API
+
+# NAS-Bench-301
+import nasbench301 as nb
+
+# Folder where the three benchmarks files are in 
+bench_folder = '~/nas_benchmark/'
+
+# Abstract Class for all Benchmarks 
+class GeneralBench: 
+
+    def get_cell(self, type=None):
+        return None
+
+    def get_arch(self, 
+                   arch=None, 
+                   train=True, 
+                   predictor_encoding=None, 
+                   cutoff=0,
+                   random_encoding='adj',
+                   deterministic=True,
+                   epochs=0,
+                   random_hash=False,
+                   max_edges=None,
+                   max_nodes=None):
+
+        arch_dict = {}
+        arch_dict['epochs'] = epochs
+
+        if arch is None:
+
+            arch = self.get_cell().random_cell(self.nasbench,
+                                               random_encoding=random_encoding, 
+                                               max_edges=max_edges, 
+                                               max_nodes=max_nodes,
+                                               cutoff=cutoff,
+                                               index_hash=self.index_hash)
+        arch_dict['spec'] = arch
+
+        if predictor_encoding:
+            arch_dict['encoding'] = self.get_cell(arch).encode(predictor_encoding=predictor_encoding,
+                                                                 nasbench=self.nasbench,
+                                                                 deterministic=deterministic,
+                                                                 cutoff=cutoff)
+
+        if train:
+            arch_dict['val_loss'] = self.get_cell(arch).get_val_loss(self.nasbench, 
+                                                                       deterministic=deterministic,
+                                                                       dataset=self.dataset)
+            arch_dict['test_loss'] = self.get_cell(arch).get_test_loss(self.nasbench,
+                                                                         dataset=self.dataset)
+            arch_dict['num_params'] = self.get_cell(arch).get_num_params(self.nasbench)
+            arch_dict['val_per_param'] = (arch_dict['val_loss'] - 4.8) * (arch_dict['num_params'] ** 0.5) / 100
+
+        return arch_dict
+
+
